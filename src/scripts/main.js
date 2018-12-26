3abe700d5170a12bdbee75f41ac8812fb5b29e90
Wesley de Souza <wesleywex@gmail.com>
2018-12-25 20:47:51 -0500

    Remove Sentry

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,3 @@
-/* globals Raven */
-Raven.config('https://652c9909de254c328378997a71c5b09f@sentry.io/171332').install();
-
 const Greeting = require('../components/Greeting/Greeting');
 
 const ComponentLoader = require('./ComponentLoader');
