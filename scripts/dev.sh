b15e972be02040d8228a9262c7c9dbd8dd8f189c
Wes Souza <wesleydesouza@me.com>
2020-07-01 17:31:50 -0400

    Small tweaks to resume

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,6 +5,7 @@ mkdir -p public
 
 cp ./src/LICENSE.txt ./public
 cp ./src/resume.html ./public
+cp ./src/resume-detailed.html ./public
 cp -a ./archive ./public
 
 parcel ./src/index.html  --out-dir ./public
