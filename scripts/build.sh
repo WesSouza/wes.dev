e916b4d2513679c2d7e783656dd488920e36df75
Wes Souza <hey@wes.dev>
2022-02-21 14:49:36 -0500

    Update parcel

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ mkdir -p ./public
 
 cp ./src/LICENSE.txt ./public/LICENSE.txt
 cp ./src/resume.html ./public/resume.html
+cp ./src/resume-detailed.html ./public/resume-detailed.html
 cp -a ./archive ./public
 
 parcel build ./src/index.html  --dist-dir ./public
