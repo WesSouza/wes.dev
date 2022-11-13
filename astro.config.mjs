9e083d183e9fb799e91a2a77eb0c932858dcc1ec
Wes Souza <noreply@wes.dev>
2022-11-13 13:42:02 -0500

    Edging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
-
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/edge';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
