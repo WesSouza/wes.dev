117b3d9a63de1eb6d2e6149753f1b0867e4582e4
Wes Souza <noreply@wes.dev>
2022-11-21 05:49:40 -0500

    Polish articles array and resume CSS

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,48 @@ export const SITE_DESCRIPTION =
   'I’m a Staff Software Engineer working at Unqork in New York. Read more about me on the website.';
 
 export const SITE_DOMAIN = 'wes.dev';
+
+export const SITE_ARTICLES = [
+  {
+    href: 'https://dev.to/wes/trpc-move-fast-and-break-nothing-usereactnyc-3g08',
+    goSlug: 'trpc-talk',
+    title: 'tRPC: Move Fast and Break Nothing',
+    subtitle: 'useReactNYC, August 2022',
+  },
+  {
+    href: 'https://dev.to/wes/simple-react-state-management-29g',
+    goSlug: 'immer-state',
+    title: 'Simple React State Management',
+    subtitle: 'DEV Community, June 2020',
+  },
+  {
+    href: 'https://dev.to/wes/opening-a-pr-a-primer-4kgc',
+    goSlug: 'pull-request',
+    title: 'Pull Requests: A Primer',
+    subtitle: 'DEV Community, February 2020',
+  },
+  {
+    href: 'https://dev.to/wes/why-i-became-and-still-am-a-web-developer-1k3',
+    goSlug: 'web-developer',
+    title: 'Why I Became, and still am, a Web Developer',
+    subtitle: 'DEV Community, May 2019',
+  },
+  {
+    href: 'https://dev.to/wes/bringing-components-to-legacy-code-2m00',
+    goSlug: 'components-talk',
+    title: 'Bringing Components to Legacy Code',
+    subtitle: 'ReactNYC, September 2017',
+  },
+  {
+    href: 'https://dev.to/wes/keeping-control-of-the-front-end-ag7',
+    goSlug: 'front-end',
+    title: 'Keeping Control of the Front-end',
+    subtitle: 'DEV Community, February 2016',
+  },
+  {
+    href: 'https://dev.to/wes/cool-things-make-people-happy-3pci',
+    goSlug: 'cool-things',
+    title: 'Cool Things Make People Happy',
+    subtitle: 'DEV Community, March 2015',
+  },
+];
