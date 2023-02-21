0c01954f816e8784ef6adbd41f0bc8fc3f5b50de
Wes Souza <noreply@wes.dev>
2023-02-21 08:48:08 -0500

    Upgrade dependencies (#75)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/edge';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wes.dev',
-  integrations: [mdx(), sitemap()],
   output: 'server',
   adapter: vercel(),
 });
