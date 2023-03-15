a075e8082858b09d7f8ed31c2479c8261cf35ba8
Wes Souza <noreply@wes.dev>
2023-03-15 09:10:59 -0400

    Let's play with SolidJS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wes.dev',
   output: 'server',
   adapter: vercel(),
+  integrations: [solidJs()]
 });
