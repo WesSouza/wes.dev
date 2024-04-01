861e6be4bc1b98325382fd9cd4b72fbe61fec30d
Wes Souza <noreply@wes.dev>
2024-03-31 20:01:10 -0400

    "Dynamic" heuristics

---
+++ src/pages/enhanced-youtube/heuristics/[version].json.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from 'astro';
+
+import { Heuristics_1 } from './_heuristics_1';
+
+export const GET: APIRoute<
+  Record<string, unknown>,
+  { version: string }
+> = async function get({ params }) {
+  const { version: _version } = params;
+
+  return new Response(JSON.stringify(Heuristics_1), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'public, max-age=600, stale-while-revalidate=60',
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+  });
+};
