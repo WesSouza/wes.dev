1cab257f2203b4ef9d46a409bd2e93578c0c7f61
Wes Souza <noreply@wes.dev>
2024-09-08 11:50:13 -0400

    Enhanced YouTube beta heuristics for new builds

--- src/pages/enhanced-youtube/heuristics/[version].json.ts
+++ src/pages/enhanced-youtube/heuristics/[version].json.ts
@@ -1,14 +1,25 @@
 import type { APIRoute } from 'astro';
 
 import Heuristics_1 from './_heuristics_1.json';
+import Heuristics_2 from './_heuristics_2.json';
 
 export const GET: APIRoute<
   Record<string, unknown>,
   { version: string }
 > = async function get({ params }) {
-  const { version: _version } = params;
+  const { version } = params;
+  const [, buildString] = version.split('_');
+  const build = buildString ? parseInt(buildString, 10) : 0;
 
-  return new Response(JSON.stringify(Heuristics_1), {
+  let heuristics: unknown;
+
+  if (build >= 18) {
+    heuristics = Heuristics_2;
+  } else {
+    heuristics = Heuristics_1;
+  }
+
+  return new Response(JSON.stringify(heuristics), {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Cache-Control': 'public, max-age=600, stale-while-revalidate=60',
