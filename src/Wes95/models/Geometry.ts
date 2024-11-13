1e3dca4538fa702539927ac48d5cfd1c5955ebad
Wes Souza <noreply@wes.dev>
2024-11-13 05:53:39 -0500

    Whoa, Windows!

---
+++ src/Wes95/models/Geometry.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+export type Point = z.infer<typeof PointSchema>;
+
+export const PointSchema = z.object({
+  x: z.number(),
+  y: z.number(),
+});
+
+export type Rect = z.infer<typeof RectSchema>;
+
+export const RectSchema = z.object({
+  x: z.number(),
+  y: z.number(),
+  width: z.number(),
+  height: z.number(),
+});
+
+export type Size = z.infer<typeof SizeSchema>;
+
+export const SizeSchema = z.object({
+  width: z.number(),
+  height: z.number(),
+});
