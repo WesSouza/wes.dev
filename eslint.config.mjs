ccc1ba69284478cd6e61885b12347757d4c0706e
Wes Souza <noreply@wes.dev>
2024-11-26 07:49:24 -0500

    ESLint: Ignore dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['.astro/**', '.vercel/**', 'public/**'],
+    ignores: ['.astro/**', '.vercel/**', 'dist/**', 'public/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
