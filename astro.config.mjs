9ee957a0d302d53bcd6ea57f6461d9313c9555bf
Wes Souza <noreply@wes.dev>
2024-11-29 11:15:57 -0500

    Package tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import solidJs from '@astrojs/solid-js';
 import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wes.dev',
   output: 'server',
-  adapter: vercel({
-    functionPerRoute: false,
-  }),
+  adapter: vercel(),
   integrations: [solidJs()],
+  vite: {
+    plugins: [
+      visualizer({
+        emitFile: true,
+        filename: 'stats.html',
+      }),
+    ],
+  },
 });
