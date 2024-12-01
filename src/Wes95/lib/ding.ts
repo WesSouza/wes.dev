d13937da8cd641212f6fe4425195a7afef2bb83a
Wes Souza <noreply@wes.dev>
2024-12-01 14:55:40 -0500

    System Tray

---
+++ src/Wes95/lib/ding.ts
@@ -0,0 +1,39 @@
+import { onCleanup, onMount } from 'solid-js';
+import { WES95_MEDIA_PATH } from '../../config';
+
+export function createDinger() {
+  let buffer: AudioBuffer | undefined;
+  let source: AudioBufferSourceNode | undefined;
+
+  const context = new AudioContext();
+
+  onMount(() => {
+    const abortController = new AbortController();
+
+    fetch(WES95_MEDIA_PATH + '/Ding.mp3', { signal: abortController.signal })
+      .then((response) => response.arrayBuffer())
+      .then((arrayBuffer) => context.decodeAudioData(arrayBuffer))
+      .then((audioBuffer) => {
+        buffer = audioBuffer;
+      });
+
+    onCleanup(() => {
+      abortController.abort();
+    });
+  });
+
+  return () => {
+    if (!buffer || !context) {
+      return;
+    }
+
+    if (source) {
+      source.stop();
+    }
+
+    source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start();
+  };
+}
