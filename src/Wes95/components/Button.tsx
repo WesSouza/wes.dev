322091173cc96b87ff8bc9aeb505723c7bd8dce6
Wes Souza <hey@wes.dev>
2026-06-28 11:19:08 +0100

    Update css95 to 0.2.0

--- src/Wes95/components/Button.tsx
+++ src/Wes95/components/Button.tsx
@@ -1,4 +1,14 @@
 import {
+  Button as Css95Button,
+  FlatButton as Css95FlatButton,
+  LinkButton as Css95LinkButton,
+  MenuButton as Css95MenuButton,
+  TaskbarButton as Css95TaskbarButton,
+  ThinButton as Css95ThinButton,
+  ToolbarButton as Css95ToolbarButton,
+  WindowTitleButton as Css95WindowTitleButton,
+} from 'css95/solid';
+import {
   createEffect,
   createMemo,
   createUniqueId,
@@ -7,7 +17,6 @@ import {
   useContext,
   type JSX,
 } from 'solid-js';
-import { Button as Css95Button } from 'css95/solid';
 import { FocusContext } from '../lib/FocusContext';
 import { WindowManager } from '../lib/WindowManager';
 import { WindowContext } from './Window';
@@ -80,41 +89,39 @@ export function Button(
     local.onFocus?.(event);
   };
 
-  const packageAppearance = createMemo(() =>
-    ['Flat', 'Menu', 'WindowTitle', undefined].includes(local.appearance),
-  );
+  const packageButton = createMemo(() => {
+    switch (local.appearance) {
+      case 'Flat':
+        return Css95FlatButton;
+      case 'Link':
+        return Css95LinkButton;
+      case 'Menu':
+        return Css95MenuButton;
+      case 'Taskbar':
+        return Css95TaskbarButton;
+      case 'Thin':
+        return Css95ThinButton;
+      case 'Toolbar':
+        return Css95ToolbarButton;
+      case 'WindowTitle':
+        return Css95WindowTitleButton;
+      default:
+        return Css95Button;
+    }
+  });
 
-  const fallbackClass = () =>
-    `${local.appearance ?? ''}Button ${mainButton() ? '-main' : ''} ${local.class ?? ''}`;
-
-  if (packageAppearance()) {
-    return (
-      <Css95Button
-        {...buttonProps}
-        flat={local.appearance === 'Flat'}
-        menu={local.appearance === 'Menu'}
-        title={local.appearance === 'WindowTitle'}
-        main={mainButton()}
-        class={local.class}
-        onBlur={handleBlur}
-        onClick={local.onClick}
-        onFocus={handleFocus}
-      >
-        {local.children}
-      </Css95Button>
-    );
-  }
+  const PackageButton = packageButton();
 
   return (
-    <button
+    <PackageButton
       {...buttonProps}
-      type="button"
-      class={fallbackClass()}
+      appearance={mainButton() ? 'main' : undefined}
+      class={local.class}
       onBlur={handleBlur}
       onClick={local.onClick}
       onFocus={handleFocus}
     >
       {local.children}
-    </button>
+    </PackageButton>
   );
 }
